af18747db56867f00bba5486d2450bd9cc5c942f
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-21 23:20:43 -0300

    feat: initial setup and basic components

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+import './bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Teste</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
